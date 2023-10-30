bed7cd138aca4ad73516026285643eaccd7a743d
Christian Cleofas <c.cleofas@psp.team>
2023-10-30 18:45:33 +0800

    NO_TICKET - Starting path

--- app/views.py
+++ app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # I-redirect ang user sa homepage
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('home')  # I-redirect ang user sa homepage
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
