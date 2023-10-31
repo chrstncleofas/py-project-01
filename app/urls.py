92b3bf2e1911e73c568daf6ecfcbafa5c4908f86
Christian Cleofas <c.cleofas@psp.team>
2023-10-31 21:38:01 +0800

    NO_TICKET - Fix UI

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login_view, name='login'),
+    path('signin', views.signin, name='signin'),
+    path('signup', views.signup, name='signup'),
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
